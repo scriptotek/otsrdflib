63881511a88eb77c9cfd4ce1337fa675ca59882d
Dan Michael O. Heggø <danmichaelo@gmail.com>
2018-01-27 19:09:09 +0100

    Rename tests to test

--- test/test_ots.py
+++ test/test_ots.py
@@ -13,7 +13,7 @@ class TestCase(unittest.TestCase):
     def test_custom_sorter(self):
 
         graph = Graph()
-        graph.load('tests/data/group_unsorted.ttl', format='turtle')
+        graph.load('test/data/group_unsorted.ttl', format='turtle')
         ots = OrderedTurtleSerializer(graph)
 
         ots.sorters = [
@@ -24,7 +24,7 @@ class TestCase(unittest.TestCase):
         ots.serialize(out)
         out = '\n'.join([x for x in out.getvalue().decode('utf-8').split('\n') if x.startswith('<')])
 
-        ref = open('tests/data/group_sorted.ttl').read()
+        ref = open('test/data/group_sorted.ttl').read()
         ref = '\n'.join([x for x in ref.split('\n') if x.startswith('<')])
 
         assert ref.strip() == out.strip()
@@ -32,7 +32,7 @@ class TestCase(unittest.TestCase):
     def test_dewey_sorter(self):
 
         graph = Graph()
-        graph.load('tests/data/dewey_unsorted.ttl', format='turtle')
+        graph.load('test/data/dewey_unsorted.ttl', format='turtle')
         ots = OrderedTurtleSerializer(graph)
 
         ots.sorters = [
@@ -44,7 +44,7 @@ class TestCase(unittest.TestCase):
         ots.serialize(out)
         out = '\n'.join([x for x in out.getvalue().decode('utf-8').split('\n') if x.startswith('<')])
 
-        ref = open('tests/data/dewey_sorted.ttl').read()
+        ref = open('test/data/dewey_sorted.ttl').read()
         ref = '\n'.join([x for x in ref.split('\n') if x.startswith('<')])
 
         assert ref.strip() == out.strip()
@@ -52,7 +52,7 @@ class TestCase(unittest.TestCase):
     def test_numeric(self):
 
         graph = Graph()
-        graph.load('tests/data/numeric_unsorted.ttl', format='turtle')
+        graph.load('test/data/numeric_unsorted.ttl', format='turtle')
         ots = OrderedTurtleSerializer(graph)
 
         ots.sorters = [
@@ -64,7 +64,7 @@ class TestCase(unittest.TestCase):
         ots.serialize(out)
         out = '\n'.join([x for x in out.getvalue().decode('utf-8').split('\n') if x.startswith('<')])
 
-        ref = open('tests/data/numeric_sorted.ttl').read()
+        ref = open('test/data/numeric_sorted.ttl').read()
         ref = '\n'.join([x for x in ref.split('\n') if x.startswith('<')])
 
         assert ref.strip() == out.strip()
@@ -72,13 +72,13 @@ class TestCase(unittest.TestCase):
     def test_mixed(self):
 
         graph = Graph()
-        graph.load('tests/data/mix_unsorted.ttl', format='turtle')
+        graph.load('test/data/mix_unsorted.ttl', format='turtle')
         ots = OrderedTurtleSerializer(graph)
         out = BytesIO()
         ots.serialize(out)
         out = '\n'.join([x for x in out.getvalue().decode('utf-8').split('\n') if x.startswith('<')])
 
-        ref = open('tests/data/mix_sorted.ttl').read()
+        ref = open('test/data/mix_sorted.ttl').read()
         ref = '\n'.join([x for x in ref.split('\n') if x.startswith('<')])
 
         assert ref.strip() == out.strip()
@@ -86,7 +86,7 @@ class TestCase(unittest.TestCase):
     def test_bnodes_sort(self):
 
         graph = Graph()
-        graph.load('tests/data/bnodes.ttl', format='turtle')
+        graph.load('test/data/bnodes.ttl', format='turtle')
         ots = OrderedTurtleSerializer(graph)
 
         out = BytesIO()
