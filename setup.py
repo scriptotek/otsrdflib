11248f8d3f6d678aa3f6164a334d371a6a1ddd5c
Dan Michael O. Heggø <danmichaelo@gmail.com>
2018-01-27 19:14:40 +0100

    Add py36, remove py33

--- setup.py
+++ setup.py
@@ -21,9 +21,9 @@ setup(name='otsrdflib',
       classifiers=[
           'Programming Language :: Python',
           'Programming Language :: Python :: 2.7',
-          'Programming Language :: Python :: 3.3',
           'Programming Language :: Python :: 3.4',
-          'Programming Language :: Python :: 3.5'
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6'
       ],
       keywords='rdflib turtle serializer',
       author='Dan Michael O. Heggø',
